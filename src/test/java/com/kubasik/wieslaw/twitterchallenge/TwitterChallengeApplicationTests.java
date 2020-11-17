705d4cac61077fcbc0b14efc018e17a24e86ab75
kubasikw <Wieslaw.Kubasik@ttsw.com.pl>
2020-11-17 18:20:44 +0100

    init commit

---
+++ src/test/java/com/kubasik/wieslaw/twitterchallenge/TwitterChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubasik.wieslaw.twitterchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
