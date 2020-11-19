4a93f572f66604ac1c7645d088edcf10d4a7ca28
kubasikw <Wieslaw.Kubasik@ttsw.com.pl>
2020-11-19 09:48:31 +0100

    remove empty line

--- src/main/java/com/kubasik/wieslaw/twitterchallenge/mappers/TweetMapper.java
+++ src/main/java/com/kubasik/wieslaw/twitterchallenge/mappers/TweetMapper.java
@@ -4,7 +4,6 @@ import com.kubasik.wieslaw.twitterchallenge.adapter.web.dto.TweetDto;
 import com.kubasik.wieslaw.twitterchallenge.domain.Tweet;
 
 public class TweetMapper {
-
     public static TweetDto mapToTweetDto(Tweet tweet) {
         return TweetDto.builder()
                 .message(tweet.getMessage())
